1e4dd830b0fd82d2c2822718ec230a2f0d389bbd
Hug <hugomcintyre@hotmail.com>
2024-11-13 19:18:17 +0000

    Seven smegment display

--- includes/seven_segment.h
+++ includes/seven_segment.h
@@ -33,13 +33,34 @@ static const unsigned int ALL_SEGMENTS[] = {SEGMENT_A, SEGMENT_B, SEGMENT_C, SEG
 
 // You will need to extend the array to include more letters.
 uint8_t values[] = {
-    0b11111100,         // 0
-    0b01100000,         // 1
-    0b11011010,         // 2
-    0b11110010,         // 3
-    0b01100110         // 4
+    0b00000000, // Nothing
+    0b11101110, // A
+    0b00111110, // B
+    0b10011100, // C
+    0b01111010, // D
+    0b10011110, // E
+    0b10001110, // F
+    0b11110110, // G
+    0b01101110, // H
+    0b00001100, // I
+    0b01111000, // J
+    0b01101110, // K
+    0b00011100, // L
+    0b10101000, // M
+    0b00101010, // N
+    0b11111100, // O
+    0b11001110, // P
+    0b11100110, // Q
+    0b00001010, // R
+    0b10110110, // S
+    0b00011110, // T
+    0b01111100, // U
+    0b00111000, // V
+    0b01010100, // W
+    0b01101110, // X
+    0b01110110, // Y
+    0b11011010  // Z
 };
-
 
 /* Initialise the GPIO pins for the seven-segment display. */
 void seven_segment_init() {
