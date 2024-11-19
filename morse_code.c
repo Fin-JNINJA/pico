a8ba36558e3725913079cf603601c2fda94fffe7
natski <59707870+natspooky@users.noreply.github.com>
2024-11-19 19:06:34 +0000

    e

--- morse_code.c
+++ morse_code.c
@@ -1,3 +1,4 @@
+//imports
 #include <stdio.h>
 #include <string.h>
 #include "pico/stdlib.h"
@@ -8,6 +9,8 @@
 #include "includes/button.h"
 #include "includes/morse_code.h"
 
+
+//global variables
 int pressed;
 int notPressed = 0;
 bool pressedInitial = false;
@@ -15,17 +18,18 @@ char morse[5] = "";
 char word[5] = "";
 bool keepActive = true;
 int valid_inputs = 0;
-
 char morseCode[26][5] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
-
 char* alphabet[] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+
 
 int main() {
 
-	setup();
+	setup(); // initializes circuits 
 
-	while (keepActive) {
+	while (keepActive) { // main loop
+		
 		pressed = 0;
+
 		while (getButtonPress()) {
 			notPressed = 0;
 			pressedInitial = true;
@@ -38,6 +42,7 @@ int main() {
 			strcat(morse, addition);
 			printf("%s\n", morse);
 		}
+
 		notPressed++;
 		checkTimeout();
 		sleep_ms(1);
@@ -46,57 +51,69 @@ int main() {
 			playExitSong();
 			printf("Your word is:  %s\nWould you like to leave?\n",word);
 		}
-			while (valid_inputs >= 4) {
-				
-				if(getButtonPress()) {
-					valid_inputs = 0;
-					LED(1);
-					memset(word, 0, strlen(word));
-				}
-				if(getButtonPress2()) {
-					LED(2);
-					exit(1);
-				}
+
+		while (valid_inputs >= 4) { // end loop
+			if(getButtonPress()) {
+				valid_inputs = 0;
+				LED(1);
+				memset(word, 0, strlen(word));
 			}
+
+			if(getButtonPress2()) {
+				LED(2);
+				exit(1);
+			}
+		}
 	}
 
 }
 
 char* checkButton() {
 	char* temp;
+
 	if(pressedInitial) {
 		if (pressed < 250) {
 			printf("Button short\n");
 			temp = ".";
 			buzzer_signal(1);
-		} else if (pressed >= 250 && pressed <= 700) {
+		}
+		
+		else if (pressed >= 250 && pressed <= 700) {
 			printf("Button long\n");
 			temp = "-";
 			buzzer_signal(2);
-		} else {
+		}
+		
+		else {
 			temp = ",";
 			buzzer_signal(3);
 		}
 	}
+
 	return temp;
 }
 
 void checkTimeout() {
 	int range = 40 * potentiometer_read(5,10);
 	if (notPressed >= range && pressedInitial) {
+
 		int index = decoder(range);
+
 		if(index < 0) {
 			printf("8\n");
 			LED(2);
 			seven_segment_show(27);
-			//error
-		}else {
+			//handles error
+		}
+		
+		else {
 			printf("%s\n", alphabet[index]);
 			strcat(word,alphabet[index]);
 			LED(1);
 			seven_segment_show(index + 1);
-			//correct
+			//handles response
 		}
+
 		memset(morse, 0, strlen(morse));
 		pressedInitial = false;
 		sleep_ms(500);
@@ -140,10 +157,12 @@ void setup() {
 
     while(temp) {
 		int read = potentiometer_read(5,10);
+
         if(read != temp_int) {
             temp_int = read;
             printf("timeout is set to: %dms \n",temp_int * 40);
         }
+		
         if(getButtonPress() || getButtonPress2()) {
             temp = false;
         }
