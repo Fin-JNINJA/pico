d9e77ba7148d9269dfe3c157102cb4fd17683b81
Hug <hugomcintyre@hotmail.com>
2024-11-21 22:08:30 +0000

    burg

--- LED.c
+++ LED.c
@@ -7,11 +7,6 @@
 #define BLUE 11 
 
 
-#define BRIGHTNESS 50
-#define LOOP_SLEEP 10
-#define MAX_COLOUR_VALUE 255
-#define MAX_PWM_LEVEL 65535
-
 
 void setup_rgb()
 {
@@ -37,26 +32,26 @@ void setup_rgb()
 
 void show_rgb(int r, int g, int b)
 {
-    pwm_set_gpio_level(RED, ~(MAX_PWM_LEVEL * r / MAX_COLOUR_VALUE * BRIGHTNESS / 100));
-    pwm_set_gpio_level(GREEN, ~(MAX_PWM_LEVEL * g / MAX_COLOUR_VALUE * BRIGHTNESS / 100));
-    pwm_set_gpio_level(BLUE, ~(MAX_PWM_LEVEL * b / MAX_COLOUR_VALUE * BRIGHTNESS / 100));
+    pwm_set_gpio_level(RED, ~(65535 * r));
+    pwm_set_gpio_level(GREEN, ~(65535 * g));
+    pwm_set_gpio_level(BLUE, ~(65535 * b));
 }
 
 void LED(int num) {
     //led input validation colours
     switch(num) {
         case 1: //A valid input turns the LED green
-            show_rgb(0,255,0);
+            show_rgb(0,1,0);
             sleep_ms(20);
             LED(0);
             break;
         case 2://An invalid input turns the LED RED
-            show_rgb(255,0,0);
+            show_rgb(1,0,0);
             sleep_ms(20);
             LED(0);
             break;
         case 3://The exit LED stays on for longer
-            show_rgb(255,0,0);
+            show_rgb(1,0,0);
             sleep_ms(500);
             LED(0);
             break;
