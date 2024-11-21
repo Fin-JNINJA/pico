d225a65407a446b05e3f3741fd42a447ca2ea1f7
Hug <hugomcintyre@hotmail.com>
2024-11-21 20:05:26 +0000

    Yippee

--- LED.c
+++ LED.c
@@ -6,11 +6,6 @@
 #define G 12 
 #define B 11 
 
-#define BRIGHTNESS 50
-#define MAX_COLOUR_VALUE 255
-#define MAX_PWM_LEVEL 65535
-
-
 void setup_rgb()
 {
     // Tell the LED pins that the PWM is in charge of its value.
@@ -21,7 +16,8 @@ void setup_rgb()
     // Figure out which slice we just connected to the LED pin, this is done for the other colors below
     uint slice_num = pwm_gpio_to_slice_num(R);
     // Get the defaults for the slice configuration. By default, the
-    // counter is allowed to wrap over its maximum range (0 to 2**16-1)
+    // counter is allowed to wrap over its maximum range
+
     pwm_config config = pwm_get_default_config();
     // Load the configuration into our PWM slice, and set it running.
     pwm_init(slice_num, &config, true);
@@ -33,34 +29,32 @@ void setup_rgb()
     pwm_init(slice_num, &config, true);
 }
 
-// Allow a colour value [0,255] and scale it up to uint16_t [0,65535].
-// Then scale it by brightness (out of 100) and invert the value (LED low is ON).
 void show_rgb(int r, int g, int b)
 {
-    pwm_set_gpio_level(R, ~r);
-    pwm_set_gpio_level(G, ~g);
-    pwm_set_gpio_level(B, ~b);
+    pwm_set_gpio_level(R, ~r*65535);
+    pwm_set_gpio_level(G, ~g*65535);
+    pwm_set_gpio_level(B, ~b*65535);
 }
 
 void LED(int num) {
     //led input validation colours
     switch(num) {
-        case 1:
-            show_rgb(0,65535,0);
+        case 1: //A valid input turns the LED green
+            show_rgb(0,1,0);
             sleep_ms(20);
             LED(0);
             break;
-        case 2:
-            show_rgb(65535,0,0);
+        case 2://An invalid input turns the LED RED
+            show_rgb(1,0,0);
             sleep_ms(20);
             LED(0);
             break;
-        case 3:
-            show_rgb(255,0,0);
+        case 3://The exit LED stays on for longer
+            show_rgb(1,0,0);
             sleep_ms(500);
             LED(0);
             break;
-        default:
+        default:// Is called to turn off the RGB
             show_rgb(0,0,0);
             break;
     }
